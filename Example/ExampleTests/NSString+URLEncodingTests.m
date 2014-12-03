e6bd6e1a798ce00f70e663b320a37fff253787e6
Piet Brauer <piet@nerdishbynature.com>
2014-12-03 08:35:21 +0100

    Add tests for NSString+URLEncoding

---
+++ Example/ExampleTests/NSString+URLEncodingTests.m
@@ -0,0 +1,34 @@
+#import <XCTest/XCTest.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
+#import "NSString+URLEncoding.h"
+
+@interface NSString_URLEncodingTests : XCTestCase
+
+@end
+
+@implementation NSString_URLEncodingTests
+
+- (void)testURLEncodedString {
+    NSString *urlToTest = @"Hallo wie gehts?";
+    expect([urlToTest xng_URLEncodedString]).to.equal(@"Hallo%20wie%20gehts%3F");
+}
+
+- (void)testURLDecodedString {
+    NSString *urlToTest = @"Hallo%20wie%20gehts%3F";
+    expect([urlToTest xng_URLDecodedString]).to.equal(@"Hallo wie gehts?");
+}
+
+- (void)testURLFromURLStringWithParameters {
+    NSURL *urlUnderTest = [NSString xng_URLFromURLString:@"https://api.xing.com/v1/conversation"
+                                              parameters:@{@"message": @"Hallo wie gehts?",
+                                                           @"unread": @(YES)}];
+    expect(urlUnderTest).to.equal([NSURL URLWithString:@"https://api.xing.com/v1/conversation?message=Hallo%20wie%20gehts%3F&unread=1"]);
+}
+
+- (void)testURLDecodedDictionaryFromString {
+    NSDictionary *dictUnderTest = [NSString xng_URLDecodedDictionaryFromString:@"message=Hallo%20wie%20gehts%3F&unread=1"];
+    expect(dictUnderTest).to.equal(@{@"message": @"Hallo%20wie%20gehts%3F", @"unread": @"1"});
+}
+
+@end
