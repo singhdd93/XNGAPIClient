eaf33f0b3f50bb17d8e83de92b3a778d852a13e1
Mark Schuette <mark.schuette@xing.com>
2015-10-14 11:32:22 +0200

    Add Events calls

---
+++ Example/ExampleTests/XNGEventsTests.m
@@ -0,0 +1,213 @@
+#import <XCTest/XCTest.h>
+#import <XNGAPIClient/XNGAPI.h>
+#import "XNGTestHelper.h"
+
+@interface XNGEventsTests : XCTestCase
+
+@property (nonatomic) XNGTestHelper *testHelper;
+
+@end
+
+@implementation XNGEventsTests
+
+- (void)setUp {
+    [super setUp];
+    self.testHelper = [[XNGTestHelper alloc] init];
+    [self.testHelper setup];
+}
+
+- (void)tearDown {
+    [super tearDown];
+    [self.testHelper tearDown];
+}
+
+- (void)testGetEvent {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] getEventForID:@"42_abcd"
+                                        userFields:@"id,page_name"
+                                      withContacts:10
+                                           success:nil
+                                           failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/events/42_abcd");
+        expect(request.HTTPMethod).to.equal(@"GET");
+
+        expect([query valueForKey:@"user_fields"]).to.equal(@"id%2Cpage_name");
+        [query removeObjectForKey:@"user_fields"];
+        expect([query valueForKey:@"with_contacts"]).to.equal(@"10");
+        [query removeObjectForKey:@"with_contacts"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+- (void)testGetEventParticipants {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] getEventParticipantsForID:@"42_abcd"
+                                                         limit:10
+                                                        offset:20
+                                                 participation:@"maybe"
+                                                    userFields:@"id,page_name"
+                                                       success:nil
+                                                       failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/events/42_abcd/guests");
+        expect(request.HTTPMethod).to.equal(@"GET");
+
+        expect([query valueForKey:@"limit"]).to.equal(@"10");
+        [query removeObjectForKey:@"limit"];
+        expect([query valueForKey:@"offset"]).to.equal(@"20");
+        [query removeObjectForKey:@"offset"];
+        expect([query valueForKey:@"participations"]).to.equal(@"maybe");
+        [query removeObjectForKey:@"participations"];
+        expect([query valueForKey:@"user_fields"]).to.equal(@"id%2Cpage_name");
+        [query removeObjectForKey:@"user_fields"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+- (void)testGetEventSearchResults {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] getEventSearchResultsForString:@"waldo"
+                                                              limit:10
+                                                           location:@"51.1084,13.6737,100"
+                                                             offset:20
+                                                         userFields:@"id,page_name"
+                                                       withContacts:10
+                                                            success:nil
+                                                            failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/events/find");
+        expect(request.HTTPMethod).to.equal(@"GET");
+
+        expect([query valueForKey:@"keywords"]).to.equal(@"waldo");
+        [query removeObjectForKey:@"keywords"];
+        expect([query valueForKey:@"limit"]).to.equal(@"10");
+        [query removeObjectForKey:@"limit"];
+        expect([query valueForKey:@"location"]).to.equal(@"51.1084%2C13.6737%2C100");
+        [query removeObjectForKey:@"location"];
+        expect([query valueForKey:@"offset"]).to.equal(@"20");
+        [query removeObjectForKey:@"offset"];
+        expect([query valueForKey:@"user_fields"]).to.equal(@"id%2Cpage_name");
+        [query removeObjectForKey:@"user_fields"];
+        expect([query valueForKey:@"with_contacts"]).to.equal(@"10");
+        [query removeObjectForKey:@"with_contacts"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+- (void)testGetEvents {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] getEventsWithLimit:10
+                                                 offset:20
+                                          participation:@"maybe"
+                                             timePeriod:@"past"
+                                             userFields:@"id,page_name"
+                                           withContacts:10
+                                                success:nil
+                                                failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/users/me/events");
+        expect(request.HTTPMethod).to.equal(@"GET");
+
+        expect([query valueForKey:@"limit"]).to.equal(@"10");
+        [query removeObjectForKey:@"limit"];
+        expect([query valueForKey:@"offset"]).to.equal(@"20");
+        [query removeObjectForKey:@"offset"];
+        expect([query valueForKey:@"participation"]).to.equal(@"maybe");
+        [query removeObjectForKey:@"participation"];
+        expect([query valueForKey:@"time_period"]).to.equal(@"past");
+        [query removeObjectForKey:@"time_period"];
+        expect([query valueForKey:@"user_fields"]).to.equal(@"id%2Cpage_name");
+        [query removeObjectForKey:@"user_fields"];
+        expect([query valueForKey:@"with_contacts"]).to.equal(@"10");
+        [query removeObjectForKey:@"with_contacts"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+- (void)testGetEventRecommendations {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] getEventRecommendationsWithLimit:10
+                                                               offset:20
+                                                           userFields:@"id,page_name"
+                                                         withContacts:10
+                                                              success:nil
+                                                              failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/events/recommendations");
+        expect(request.HTTPMethod).to.equal(@"GET");
+
+        expect([query valueForKey:@"limit"]).to.equal(@"10");
+        [query removeObjectForKey:@"limit"];
+        expect([query valueForKey:@"offset"]).to.equal(@"20");
+        [query removeObjectForKey:@"offset"];
+        expect([query valueForKey:@"user_fields"]).to.equal(@"id%2Cpage_name");
+        [query removeObjectForKey:@"user_fields"];
+        expect([query valueForKey:@"with_contacts"]).to.equal(@"10");
+        [query removeObjectForKey:@"with_contacts"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+- (void)testGetEventsOfContacts {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] getEventsOfContactsWithLimit:10
+                                                           offset:20
+                                                       userFields:@"id,page_name"
+                                                     withContacts:10
+                                                          success:nil
+                                                          failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/events/contacts");
+        expect(request.HTTPMethod).to.equal(@"GET");
+
+        expect([query valueForKey:@"limit"]).to.equal(@"10");
+        [query removeObjectForKey:@"limit"];
+        expect([query valueForKey:@"offset"]).to.equal(@"20");
+        [query removeObjectForKey:@"offset"];
+        expect([query valueForKey:@"user_fields"]).to.equal(@"id%2Cpage_name");
+        [query removeObjectForKey:@"user_fields"];
+        expect([query valueForKey:@"with_contacts"]).to.equal(@"10");
+        [query removeObjectForKey:@"with_contacts"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+- (void)testPutEventParticipation {
+    [self.testHelper executeCall:^{
+        [[XNGAPIClient sharedClient] putEventParticipationForID:@"42_abcd"
+                                                  participation:@"maybe"
+                                                        success:nil
+                                                        failure:nil];
+    } withExpectations:^(NSURLRequest *request, NSMutableDictionary *query, NSMutableDictionary *body) {
+        expect(request.URL.host).to.equal(@"api.xing.com");
+        expect(request.URL.path).to.equal(@"/v1/events/42_abcd/rsvp");
+        expect(request.HTTPMethod).to.equal(@"PUT");
+
+        expect([body valueForKey:@"selected"]).to.equal(@"maybe");
+        [body removeObjectForKey:@"selected"];
+
+        expect([query allKeys]).to.haveCountOf(0);
+        expect([body allKeys]).to.haveCountOf(0);
+    }];
+}
+
+@end
---
+++ XNGAPIClient/XNGAPIClient+Events.m
@@ -0,0 +1,182 @@
+#import "XNGAPIClient+Events.h"
+
+@implementation XNGAPIClient (Events)
+
+- (void)getEventForID:(NSString *)eventID
+           userFields:(NSString *)userFields
+         withContacts:(NSInteger)numberOfAttendingContacts
+              success:(void (^)(id))success
+              failure:(void (^)(NSError *))failure {
+    if (!eventID) {
+        return;
+    }
+
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    if (userFields) {
+        parameters[@"user_fields"] = userFields;
+    }
+    if (numberOfAttendingContacts) {
+        parameters[@"with_contacts"] = @(numberOfAttendingContacts);
+    }
+
+    NSString *path = [NSString stringWithFormat:@"v1/events/%@", eventID];
+    [self getJSONPath:path parameters:parameters success:success failure:failure];
+}
+
+- (void)getEventParticipantsForID:(NSString *)eventID
+                            limit:(NSInteger)limit
+                           offset:(NSInteger)offset
+                    participation:(NSString *)participation
+                       userFields:(NSString *)userFields
+                          success:(void (^)(id))success
+                          failure:(void (^)(NSError *))failure {
+    if (!eventID) {
+        return;
+    }
+
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    if (limit) {
+        parameters[@"limit"] = @(limit);
+    }
+    if (offset) {
+        parameters[@"offset"] = @(offset);
+    }
+    if (participation) {
+        parameters[@"participations"] = participation;
+    }
+    if (userFields) {
+        parameters[@"user_fields"] = userFields;
+    }
+
+    NSString *path = [NSString stringWithFormat:@"v1/events/%@/guests", eventID];
+    [self getJSONPath:path parameters:parameters success:success failure:failure];
+}
+
+- (void)getEventSearchResultsForString:(NSString *)searchString
+                                 limit:(NSInteger)limit
+                              location:(NSString *)location
+                                offset:(NSInteger)offset
+                            userFields:(NSString *)userFields
+                          withContacts:(NSInteger)numberOfAttendingContacts
+                               success:(void (^)(id))success
+                               failure:(void (^)(NSError *))failure {
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    if (searchString) {
+        parameters[@"keywords"] = searchString;
+    }
+    if (limit) {
+        parameters[@"limit"] = @(limit);
+    }
+    if (location) {
+        parameters[@"location"] = location;
+    }
+    if (offset) {
+        parameters[@"offset"] = @(offset);
+    }
+    if (userFields) {
+        parameters[@"user_fields"] = userFields;
+    }
+    if (numberOfAttendingContacts) {
+        parameters[@"with_contacts"] = @(numberOfAttendingContacts);
+    }
+
+    NSString *path = @"v1/events/find";
+    [self getJSONPath:path parameters:parameters success:success failure:failure];
+}
+
+- (void)getEventsWithLimit:(NSInteger)limit
+                    offset:(NSInteger)offset
+             participation:(NSString *)participation
+                timePeriod:(NSString *)timePeriod
+                userFields:(NSString *)userFields
+              withContacts:(NSInteger)numberOfAttendingContacts
+                   success:(void (^)(id))success
+                   failure:(void (^)(NSError *))failure {
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    if (limit) {
+        parameters[@"limit"] = @(limit);
+    }
+    if (offset) {
+        parameters[@"offset"] = @(offset);
+    }
+    if (participation) {
+        parameters[@"participation"] = participation;
+    }
+    if (timePeriod) {
+        parameters[@"time_period"] = timePeriod;
+    }
+    if (userFields) {
+        parameters[@"user_fields"] = userFields;
+    }
+    if (numberOfAttendingContacts) {
+        parameters[@"with_contacts"] = @(numberOfAttendingContacts);
+    }
+
+    NSString *path = @"v1/users/me/events";
+    [self getJSONPath:path parameters:parameters success:success failure:failure];
+}
+
+- (void)getEventRecommendationsWithLimit:(NSInteger)limit
+                                  offset:(NSInteger)offset
+                              userFields:(NSString *)userFields
+                            withContacts:(NSInteger)numberOfAttendingContacts
+                                 success:(void (^)(id))success
+                                 failure:(void (^)(NSError *))failure {
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    if (limit) {
+        parameters[@"limit"] = @(limit);
+    }
+    if (offset) {
+        parameters[@"offset"] = @(offset);
+    }
+    if (userFields) {
+        parameters[@"user_fields"] = userFields;
+    }
+    if (numberOfAttendingContacts) {
+        parameters[@"with_contacts"] = @(numberOfAttendingContacts);
+    }
+
+    NSString *path = @"v1/events/recommendations";
+    [self getJSONPath:path parameters:parameters success:success failure:failure];
+}
+
+- (void)getEventsOfContactsWithLimit:(NSInteger)limit
+                              offset:(NSInteger)offset
+                          userFields:(NSString *)userFields
+                        withContacts:(NSInteger)numberOfAttendingContacts
+                             success:(void (^)(id))success
+                             failure:(void (^)(NSError *))failure {
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    if (limit) {
+        parameters[@"limit"] = @(limit);
+    }
+    if (offset) {
+        parameters[@"offset"] = @(offset);
+    }
+    if (userFields) {
+        parameters[@"user_fields"] = userFields;
+    }
+    if (numberOfAttendingContacts) {
+        parameters[@"with_contacts"] = @(numberOfAttendingContacts);
+    }
+
+    NSString *path = @"v1/events/contacts";
+    [self getJSONPath:path parameters:parameters success:success failure:failure];
+}
+
+- (void)putEventParticipationForID:(NSString *)eventID
+                     participation:(NSString *)participation
+                           success:(void (^)(id))success
+                           failure:(void (^)(NSError *))failure {
+    if (!eventID || !participation) {
+        return;
+    }
+
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"selected"] = participation;
+
+    NSString *path = [NSString stringWithFormat:@"v1/events/%@/rsvp", eventID];
+    [self putJSONPath:path parameters:parameters success:success failure:success];
+}
+
+@end
